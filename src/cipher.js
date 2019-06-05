03de8b3e79a84ac00e2d53f316d07611a15fb7dc
Nienorloth <nienorloth@gmail.com>
2019-06-05 18:47:46 -0500

    Se modificó el estilo para ver dos divs simultaneamente y probar funcionalidad, se agregan las funciones de cipher: encode y decode, se eliminan líneas de test del html, se agregan funciones para definir el offset según los animales elegidos

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,54 @@
+
 window.cipher = {
-  // ... 
+    encode: function(){
+        let arr = [];
+        for (let i = 0; i < x.length; i++) {
+            if (x.charCodeAt(i) === 32) {
+            arr.push(" ");
+            } else if (x.charCodeAt(i) === 46){
+            arr.push(".");
+            } else if (x.charCodeAt(i) === 44){
+            arr.push(",");
+            } else if (x.charCodeAt (i) === 59){
+            arr.push(";");
+            } else if (x.charCodeAt (i) === 58){
+            arr.push(":");
+            } else if (x.charCodeAt(i) < 97) {
+            let asc =  (x.charCodeAt(i)- 65 + y)%26+65;  
+            arr.push(String.fromCharCode(asc));
+            } else {
+            let asc = (x.charCodeAt(i)-97 + y)%26+97;
+            arr.push(String.fromCharCode(asc));
+            }
+            let ciphered = arr.join("");
+            return ciphered
+        }
+    },
+    
+    decode: function(){
+        let arr = [];
+        for (let i = 0; i < x.length; i++) {
+            if (x.charCodeAt(i) === 32) {
+            arr.push(" ");
+            } else if (x.charCodeAt(i) === 46){
+            arr.push(".");
+            } else if (x.charCodeAt(i) === 44){
+            arr.push(",");
+            } else if (x.charCodeAt (i) === 59){
+            arr.push(";");
+            } else if (x.charCodeAt (i) === 58){
+            arr.push(":");
+            } else if (x.charCodeAt(i) < 97) {
+            let asc =  (x.charCodeAt(i)- 65 - y)%26+65;  
+            arr.push(String.fromCharCode(asc));
+            } else {
+            let asc = (x.charCodeAt(i)-97 - y)%26+97;
+            arr.push(String.fromCharCode(asc));
+            }
+            let deciphered = arr.join("");
+            return deciphered
+        }
+    }
+    
 };
+*/
--- src/index.js
+++ src/index.js
@@ -1,1 +1,61 @@
-console.log("hola mundo");
+let avatar = 0;
+let x = 0;
+let y = 0;
+const offOwl = document.getElementById("offowl");
+const offCat = document.getElementById("offcat");
+const offButter = document.getElementById("offbutter");
+
+document.getElementById("owl").addEventListener("click", owlAvatar);
+document.getElementById("cat").addEventListener("click", catAvatar);
+document.getElementById("butter").addEventListener("click", butterAvatar);
+
+function owlAvatar(){
+    avatar = 1;
+    alert("buho " + avatar);
+    defineOffset(1);
+}
+function catAvatar(){
+    avatar = 2;
+    alert("gato " + avatar);
+    defineOffset(2);
+}
+function butterAvatar(){
+    avatar = 3;
+    alert("mariposa " + avatar);
+    defineOffset(3);
+}
+
+function defineOffset(avatar){
+    console.log(avatar)
+    if (avatar === 1){
+    offOwl.style.display="none";
+    offCat.addEventListener("click", () => {
+        y = 3;
+        alert("buho y gato " + y);
+        });
+     offButter.addEventListener("click", () => {
+        y = 5;
+        alert("buho y mariposa " +y);
+        });
+    } else if (avatar === 2){
+    offCat.style.display="none";
+    offOwl.addEventListener("click", () => {
+        y = 3;
+        alert("gato y buho " + y);
+        });
+     offButter.addEventListener("click", () => {
+        y = 7;
+        alert("gato y mariposa " + y);
+        });
+    } else if (avatar === 3){
+    offButter.style.display="none";
+    offOwl.addEventListener("click", () => {
+        y = 5;
+        alert("mariposa y buho " + y);
+        });
+     offCat.addEventListener("click", () => {
+        y = 7;
+        alert("mariposa y gato " + y);
+        });
+    }
+}
